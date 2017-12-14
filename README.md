92ce04670b3194c3f33df1caa0c98c0e2ed45cc0
pvidal <pablo.vidal@almis.com>
2017-12-14 15:59:25 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Project with GIT FLOW
+This project uses gitflow. Awesome!
+
+Also uses `maven gitflow plugins!`
---
+++ src/main/java/com/example/demo/GitFlowApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFlowApplication.class, args);
+		
+		System.out.print("Bye");
+	}
+}
